a4bc130ac39fd4d6ee1fc589f3243fb8b0534528
Klim Birukov <klimbirukov@gmail.com>
2022-12-07 16:23:45 +0300

    Removed unused imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post, UseGuards} from "@nestjs/common";
+import {Body, Controller, Post, UseGuards} from "@nestjs/common";
 
 import {UsersService} from "./users.service";
 import {CreateUserDto} from "./dto/createUser.dto";
